46c03143faa126679a11b17affb4e06053e94b3b
Richard Hart <richardhart92@gmail.com>
2015-11-23 22:32:09 -0500

    Continued working on classes for points and segments. Started working on class for graphs.

---
+++ Graph.java
@@ -0,0 +1,19 @@
+public class Graph {
+	private Segment[] segments;
+
+	public Graph() {
+		//TODO: randomly generate segments for testing
+		//Make sure no segments overlap
+	}
+
+	public Graph(Segment[] p_segments) {
+		segments = p_segments;
+	}
+
+	public Point[] find_intersection_points() {
+		//TODO: Run the algorithm here to find the intersection points		
+
+		return null;
+	}
+
+}
