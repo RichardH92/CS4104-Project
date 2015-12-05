677881d88f61059f30878708736352f97e3504a1
Richard Hart <richardhart92@gmail.com>
2015-12-05 14:06:11 -0500

    Wrote some pseudo code for the algo solver.

---
+++ Algo_Solver.java
@@ -0,0 +1,26 @@
+public class Algo_Solver {
+	//private Queue of events
+	//private Tree of segments
+
+	public Algo_Solver(Graph g) {
+		//TODO: Preprocess shit
+	}
+
+	public List<Point> find_intersection_points() {
+		//while queue not empty
+	}
+
+	private void preprocess_segments(segment[] segments) {
+		//Make segments into events
+	}
+
+	private Endpoint_Event make_event() {
+
+	}
+
+	private void handle_event(Endpoint_Event e) {
+		//Switch event type
+	}
+
+
+}
