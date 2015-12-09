68771aebde7ff74548eec587838bb8ab2d78a10f
Robert Blissard <blissard@gum.rlogin>
2015-12-09 17:09:59 -0500

    Added file generator that outputs summary of tests

--- Test_Segment.java
+++ Test_Segment.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 public class Test_Segment {
 
 	public static boolean Test_Intersection_True() {
@@ -61,7 +62,7 @@ public class Test_Segment {
 		Point b2 = new Point(6, 1);
 
 		Segment s = new Segment(a1, a2);
-		Segment t = new Segment(b1, b2);	
+		Segment t = new Segment(b1, b2);
 
 
 		boolean ret = true;
@@ -79,7 +80,7 @@ public class Test_Segment {
 		else
 			System.out.println("Fail\r\n");
 
-		return ret;			
+		return ret;
 	}
 
 	public static boolean Test_Intersection_Equal_Endpoints() {
@@ -92,7 +93,7 @@ public class Test_Segment {
 		Point b2 = new Point(0, 4);
 
 		Segment s = new Segment(a1, a2);
-		Segment t = new Segment(b1, b2);	
+		Segment t = new Segment(b1, b2);
 
 
 		boolean ret = true;
@@ -110,7 +111,7 @@ public class Test_Segment {
 		else
 			System.out.println("Fail\r\n");
 
-		return ret;			
+		return ret;
 	}
 
 	public static boolean Test_Intersection_Overlap_Endpoint() {
@@ -123,7 +124,7 @@ public class Test_Segment {
 		Point b2 = new Point(1, 4);
 
 		Segment s = new Segment(a1, a2);
-		Segment t = new Segment(b1, b2);	
+		Segment t = new Segment(b1, b2);
 
 
 		boolean ret = true;
@@ -136,7 +137,7 @@ public class Test_Segment {
 		else
 			System.out.println("Fail\r\n");
 
-		return ret;	
+		return ret;
 
 	}
 
@@ -162,7 +163,7 @@ public class Test_Segment {
 		else
 			System.out.println("Fail\r\n");
 
-		return ret;	
+		return ret;
 	}
 
 	public static boolean Test_Overlaps_True() {
@@ -187,7 +188,7 @@ public class Test_Segment {
 		else
 			System.out.println("Fail\r\n");
 
-		return ret;	
+		return ret;
 	}
 
 	public static boolean Test_Overlaps_False() {
@@ -212,7 +213,7 @@ public class Test_Segment {
 		else
 			System.out.println("Fail\r\n");
 
-		return ret;	
+		return ret;
 	}
 
 	public static boolean Test_Overlaps_Equal_Endpoints() {
@@ -237,27 +238,65 @@ public class Test_Segment {
 		else
 			System.out.println("Fail\r\n");
 
-		return ret;	
+		return ret;
 	}
 
 	public static void Test_Running_Time_For_n_Segments(int n) {
+		String filename = "IntersectionsFor" + n + "Segments.txt";
 		System.out.println("---------------------------------\n");
-		Graph graph = new Graph(n);
+		System.out.println("Outputting to " + filename);
+		File file = new File(filename);
+		try {
+			file.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-		long startTime = System.nanoTime();
-		List<Point> bruteForce_i_points = Brute_Force_Solver.solve(graph);
-		long endTime = System.nanoTime();
+		try {
+			PrintWriter writer = new PrintWriter(filename, "UTF-8");
+			Graph graph = new Graph(n);
 
-		long totalTime = (endTime - startTime) / 1000000;
+			long startTime = System.nanoTime();
+			List<Point> bruteForce_i_points = Brute_Force_Solver.solve(graph);
+			long endTime = System.nanoTime();
 
-		System.out.println("Brute force algorithm with " + n + " segments ran for " + totalTime + " milliseconds\nReported " + bruteForce_i_points.size() / 2 + " intersection points.\n");
+			long totalTime = (endTime - startTime) / 1000000;
+			String bruteForceRunTime = "Brute force algorithm with " + n + " segments ran for " + totalTime + " milliseconds\nReported " + bruteForce_i_points.size() / 2 + " intersection points.\n";
+			System.out.println(bruteForceRunTime);
 
-		Algo_Solver solver = new Algo_Solver(graph);
-		startTime = System.nanoTime();
-		List<Point> algo_i_points = solver.find_intersection_points();
-		endTime = System.nanoTime();
+			Algo_Solver solver = new Algo_Solver(graph);
+			startTime = System.nanoTime();
+			List<Point> algo_i_points = solver.find_intersection_points();
+			endTime = System.nanoTime();
 
-		totalTime = (endTime - startTime) / 1000000;
-		System.out.println("Our algorithm with " + n + " segments ran for " + totalTime + " milliseconds\nReported " + algo_i_points.size() + " intersection points.\n");
+			totalTime = (endTime - startTime) / 1000000;
+			String algorithmRunTime = "Our algorithm with " + n + " segments ran for " + totalTime + " milliseconds\nReported " + algo_i_points.size() + " intersection points.\n";
+			System.out.println(algorithmRunTime);
+			
+			writer.println("Summary:");
+			writer.println(bruteForceRunTime);
+			writer.println(algorithmRunTime);
+			writer.println("Segments:");
+			for (Segment segment : graph.get_segments()) {
+				writer.println(segment);
+			}
+			writer.println("\n");
+			writer.println("Intersections:");
+			for (Point point : algo_i_points) {
+				writer.println(point);
+			}
+			writer.close();
+
+
+		} catch (FileNotFoundException | UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+
+
+
+
+
+
 	}
 }
